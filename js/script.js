560ad9cb048666b15ae992fc4059f91a67631978
henrik3 <henriqlh@gmail.com>
2021-02-11 10:49:42 -0300

    .

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let head = document.getElementsByTagName("head")[0]
+
+let link = document.createElement('link')
+
+link.rel = 'stylesheet'
+link.type = 'text/css'
+link.href = 'css/style.css'
+
+head.appendChild(link)
